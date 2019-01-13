ed58627e5a09b996481a858037c2aa3773022e31
GarrettMatthews <46498635+GarrettMatthews@users.noreply.github.com>
2019-01-13 13:29:28 -0700

    Delete git_pull.sh

--- git_pull.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-# for pulling git repositories
-SOURCEDIR=/home/pi/Desktop/Git_Repositories/
-for i in /Git_Repositories; do [ -d $i ] && cd "$i" && echo "---Pulling $i"; git pull; echo "---Update Complete"; done
