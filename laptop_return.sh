7debe8d4a5f4578be0f532a0da093353a2c8c449
Garrett Matthews <46498635+GarrettMatthews@users.noreply.github.com>
2020-02-22 08:21:28 -0800

    Add files via upload

---
+++ laptop_return.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# Script for returning to local laptop user
+
+ssh -Y garrettmatthews@10.42.0.1
---
+++ raspberry.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+# For loading into the raspberry pi terminal
+
+ssh -Y pi@10.42.0.55
